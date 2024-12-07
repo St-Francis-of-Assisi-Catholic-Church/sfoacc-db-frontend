9ae1fce9c97989efd2b33effcefb64f6f081745b
johnkdunyo <johnkdunyo@gmail.com>
2024-12-07 12:35:40 +0000

    code clean up

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+import { setPageTitle } from "../utils/seo";
+
 export function Dashboard() {
+  useEffect(() => {
+    setPageTitle("Dashboard");
+  }, []);
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>Dashboard Page</h1>
     </div>
   );
 }
